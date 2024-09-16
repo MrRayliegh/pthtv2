33d781e0fefc0c87e2d3043554c4f422b1fa3df9
MrRayliegh <edwardcole143@gmail.com>
2024-09-16 21:16:16 +0800

    Update pthtv2ac.lua

--- pthtv2ac.lua
+++ pthtv2ac.lua
@@ -1,3 +1,25 @@
 return {
-  487237 -- OWNER
+    487237, -- OWNER
+    296882, -- COA
+    259177, -- 3z
+    782878, -- NAPI
+    169778, -- NAPI
+    453744, -- nuwaa 
+    782707, -- Avarice
+    40446,  -- ppJake
+    824167, -- XinNuo
+    803578, -- XinNuo2
+    368010,  -- Matho Rin
+    254632,  -- fawwazy
+    589703,  -- Cha
+    226193,  -- marco
+    402886, -- Booster
+    6588,  -- Booster 2
+    24451, -- ! S
+    11805, --! S2
+    83493, -- Ba1lBoom
+    555228,-- elbert1
+    784662, -- elbert2 
+    533297, -- nyarc
+    496177  -- xwe
 }
